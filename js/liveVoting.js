02504bd222a8923e24cdbc9b759558da1e0e9257
Barry Coolen <barrycoolen@gmail.com>
2013-10-07 21:35:49 -0300

    Move live voting js to its own file
    - Move inline js from index.html to a new file called js/liveVoting.js.
      This file will contain all the application-specific code.

---
+++ js/liveVoting.js
@@ -0,0 +1,32 @@
+﻿// Define the URL needed to connect to GoInstant.
+var goInstantUrl = "https://goinstant.net/BarelyCool/LiveVote";
+
+$(document).ready(function()
+{
+    // Attempt to connect to the GoInstant server.
+    goinstant.connect(goInstantUrl, function (err, platform, lobby)
+    {
+        // Determine if there was an error connecting to the GoInstant server.
+        if (err)
+        {
+            // Something went wrong connecting to the GoInstant server.  Without
+            // the connection the app cannot function, so alert the user by
+            // displaying a message and disabling all the input fields on the
+            // page.
+            throw err;
+        }
+
+        var entry = lobby.key("entry");
+        var el = $("input[name='entry']");
+
+        entry.on("set", function(value, context)
+        {
+            el.val(value);
+        });
+
+        el.on("keyup", function()
+        {
+            entry.set($(this).val());
+        });
+    });
+});
