c36f4294989233c4ac26d68362d4f41c9ef522d5
Barry Coolen <barrycoolen@gmail.com>
2013-10-08 00:09:04 -0300

    Add handlers and watch listner
    - Add handler for form submission, which invokes the addEntry() function
      and will eventually add entries to the ballot.
    - Add watch listener for the ballot so that it can eventually react to
      entries being added and removed from the ballot.
    - Add convenience method called handleError() for logging errors,
      disabling the application and displaying a user-friendly error message
      to the user.

--- js/liveVote.js
+++ js/liveVote.js
@@ -9,39 +9,68 @@ $(document).ready(function()
     console.log("Attempting to connect to the GoInstant server.");
 
     // Attempt to connect to the GoInstant server.
-    goinstant.connect(goInstantUrl, function (err, platform, room)
+    goinstant.connect(goInstantUrl, function (error, platform, room)
     {
         // Determine if there was an error connecting to the GoInstant server.
-        if (err)
+        if (error)
         {
             // Something went wrong connecting to the GoInstant server.  Without
             // the connection the app cannot function, so take appropriate
             // action.
-            console.log("Error connecting to GoInstant server: " + err);
-
-            // Disable the application by disabling all of the input fields on
-            // the page.  This will prevent the user from doing anything.
-            $("input").prop("disabled", true);
-
-            // Let the user know that a problem occurred and that the
-            // application is now disabled.
-            //
-            // TODO An alert is okay for now, but since they're generally
-            // annoying it would be nice to present this message a different
-            // way.
-            //
-            alert("Unable to connect to GoInstant server.  Application is "
-                + "disabled.");
-
-            // For good measure throw the error.
-            throw err;
+            handleError(
+                error,
+                "Unable to connect to GoInstant.  Application is disabled.");
         }
 
         // The connection with GoInstant was succesful, so now we can proceed
         // with initializing and configuring the application.
         console.log("Successfully connected to the GoInstant server.");
 
-        // TODO Fill out the rest of the logic...
+        // Get the ballot key from the current room.
+        var ballot = room.key("/ballot");
+
+        // Set up a handler for when the entry form is submitting.  The handler
+        // will invoke the addEntry() function with the ballot object as well as
+        // the entry value that was submitted by the user.
+        $("#addEntry").submit(function(event)
+        {
+            // Get the value of the entry that was submitted by the user.
+            var entry = $("#entry").val();
+
+            // Add the entry to the current ballot.
+            addEntry(entry, ballot);
+
+            // Prevent the rest of the form submission from happening so that
+            // the browser doesn't reload the page.
+            event.preventDefault();
+        });
+
+        // Create a listener that will be triggered when any user, local or
+        // remote, performs any action on the ballot (such as add/remove entry).
+        var ballotListener = function(value, context)
+        {
+            console.log("Ballot listener invoked...");
+
+            // TODO Fill out the listener...
+        };
+
+        // Set up a watch on the ballot key and supply a listener that will
+        // respond to entries being added or removed from the ballot.
+        ballot.watch(ballotListener, function(error)
+        {
+            console.log("Attempting to setup ballot watch.");
+
+            // Determine if there was an error setting up the ballot watch.
+            if (error)
+            {
+                // An error occurred setting up the ballot watch.  Without the
+                // watch the application is effectively useless, so log the
+                // error and disable the application.
+                handleError(error, "Unable to setup ballot watch.");
+            }
+
+            console.log("Successfully setup ballot watch.");
+        });
     });
 });
 
@@ -57,7 +86,42 @@ $(document).ready(function()
  */
 function addEntry(entry, ballot)
 {
-    console.log("Attempting to add entry='" + entry + "' to ballet");
+    console.log("Attempting to add entry='" + entry + "' to ballot.");
 
     // TODO Fill out the rest of this function...
+}
+
+/**
+ * Invoked when an error occurs initializing the application.  The error will be
+ * logged along with a user-friendly description that will be shown to the user.
+ * All input fields on the page will be disabled since the application can't
+ * function with errors.
+ *
+ * @param err
+ *          The application error that was encountered.  Can't be undefined.
+ * @param desc
+ *          A user-friendly description of the error that was encountered.  This
+ *          description will be logged to the console for debugging purposes and
+ *          will also be displayed to the user.  Can't be an empty string.
+ */
+function handleError(err, desc)
+{
+    // Log the user-friendly description of the error to the console for
+    // debugging purposes.
+    console.log(desc);
+
+    // Disable the application by disabling all of the input fields on the page.
+    // This will prevent the user from doing anything.
+    $("input").prop("disabled", true);
+
+    // Let the user know that a problem occurred and that the application is now
+    // disabled.
+    //
+    // TODO An alert is okay for now, but since they're generally annoying it
+    // would be nice to present this message a different way.
+    //
+    alert(desc);
+
+    // For good measure throw the error.
+    throw err;
 }
